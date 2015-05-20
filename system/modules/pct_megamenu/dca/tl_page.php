e393fff1c41f9f10cb428d1724edecd5f0f4e586
Tim Gatzky <info@tim-gatzky.de>
2015-05-20 11:49:40 +0200

    find all articles in the current page tree

--- system/modules/pct_megamenu/dca/tl_page.php
+++ system/modules/pct_megamenu/dca/tl_page.php
@@ -54,7 +54,7 @@ $GLOBALS['TL_DCA']['tl_page']['fields']['pct_mm_article'] = array
 	'label'                   => &$GLOBALS['TL_LANG']['tl_page']['pct_mm_article'],
 	'exclude'                 => true,
 	'inputType'               => 'select',
-	'options_callback'        => array('tl_content', 'getArticles'),
+	'options_callback'        => array('tl_page_pct_megamenu', 'getArticles'),
 	'eval'                    => array('includeBlankOption'=>true, 'chosen'=>true, 'submitOnChange'=>true),
 	'wizard' => array
 	(
@@ -84,3 +84,75 @@ $GLOBALS['TL_DCA']['tl_page']['fields']['pct_mm_floating'] = array
 	'reference'               => &$GLOBALS['TL_LANG']['MSC'],
 	'sql'                     => "varchar(32) NOT NULL default ''"
 );
+
+
+/**
+ * Class tl_page_pct_megamenu
+ */
+class tl_page_pct_megamenu extends \Backend
+{
+	/**
+	 * Get all articles and return them as array (article teaser)
+	 * @param \DataContainer
+	 * @return array
+	 */
+	public function getArticles(\DataContainer $objDC)
+	{
+		$this->import('BackendUser','User');
+		$arrPids = array();
+		$arrArticle = array();
+		$arrRoot = array();
+		$intPid = $objDC->activeRecord->pid;
+
+		if (\Input::get('act') == 'overrideAll')
+		{
+			$intPid= \Input::get('id');
+		}
+
+		$objDatabase = \Database::getInstance();
+		$objPage = \PageModel::findWithDetails($objDC->id);
+		$arrRoot = $objDatabase->getChildRecords($objPage->rootId, 'tl_page');
+		array_unshift($arrRoot, $objPage->rootId);
+		
+		// Limit pages to the user's pagemounts
+		if ($this->User->isAdmin)
+		{
+			$objArticle = $objDatabase->execute("SELECT a.id, a.pid, a.title, a.inColumn, p.title AS parent FROM tl_article a LEFT JOIN tl_page p ON p.id=a.pid" . (!empty($arrRoot) ? " WHERE a.pid IN(". implode(',', array_map('intval', array_unique($arrRoot))) .")" : "") . " ORDER BY parent, a.sorting");
+		}
+		else
+		{
+			foreach ($this->User->pagemounts as $id)
+			{
+				if (!in_array($id, $arrRoot))
+				{
+					continue;
+				}
+
+				$arrPids[] = $id;
+				$arrPids = array_merge($arrPids, $objDatabase->getChildRecords($id, 'tl_page'));
+			}
+
+			if (empty($arrPids))
+			{
+				return $arrArticle;
+			}
+
+			$objArticle = $objDatabase->execute("SELECT a.id, a.pid, a.title, a.inColumn, p.title AS parent FROM tl_article a LEFT JOIN tl_page p ON p.id=a.pid WHERE a.pid IN(". implode(',', array_map('intval', array_unique($arrPids))) .") ORDER BY parent, a.sorting");
+		}
+
+		// Edit the result
+		if ($objArticle->numRows)
+		{
+			\System::loadLanguageFile('tl_article');
+
+			while ($objArticle->next())
+			{
+				$key = $objArticle->parent . ' (ID ' . $objArticle->pid . ')';
+				$arrArticle[$key][$objArticle->id] = $objArticle->title . ' (' . ($GLOBALS['TL_LANG']['tl_article'][$objArticle->inColumn] ?: $objArticle->inColumn) . ', ID ' . $objArticle->id . ')';
+			}
+		}
+
+		return $arrArticle;
+	}
+
+}
